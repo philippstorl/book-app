8235bb9b8a0848d0028436c5b159007a0f0b2b72
Philipp Storl <philipp@staffbase.com>
2020-06-23 19:18:35 +0200

    Add BookList component & Semantic UI React

---
+++ src/components/BookList/BookList.tsx
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from 'react';
+import {List} from 'semantic-ui-react'
+
+import BookItem, {Book} from './BookItem';
+
+export default function () {
+    const [books, setBooks] = useState([]);
+
+    const fetchData = () => {
+        try {
+            fetch('http://localhost:4730/books')
+                .then((response) => response.json())
+                .then((books) => {
+                    setBooks(books);
+                });
+        } catch (error) {
+            console.log('error fetching books', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <List divided verticalAlign='middle'>
+            {books.map((book: Book) => {
+                return (
+                    <BookItem key={book.isbn} {...book} />
+                );
+            })}
+        </List>
+    );
+};
